39ea1bdf9b2c0b151d1390721849c4dab4c4a40d
andres-dalisay <21106151+andres-dalisay@users.noreply.github.com>
2025-03-19 01:07:40 +0800

    refactor: transferred functions from Gameboard to GameController class

--- script.js
+++ script.js
@@ -19,65 +19,90 @@ const Gameboard = (function () {
         });
     }
 
-    function placePiece (piece, cellX, cellY) {
-        const idxX = cellX - 1;
-        const idxY = cellY - 1;
+    return {getBoard, displayBoard, resetBoard};
+})();
 
-        if (isValidMove(piece, idxX, idxY)) {
-            board[idxX][idxY] = piece;
-            console.log(Gameboard.gameWon(idxX, idxY));
-        } else {
-            console.log("Invalid move.");
-        }
+const Player = function (playerNum) {
+    const piece = (playerNum === 1) ? "O" : "X";
+    return {playerNum, piece};
+}
+
+const GameController = (function () {
+    let currentPlayer;
+
+    const playerOne = Player(1);
+    const playerTwo = Player(2);
+
+    function placePiece (player, cellX, cellY) {
+        const idxX = cellX;
+        const idxY = cellY;
+
+        Gameboard.getBoard()[idxX][idxY] = player;
     }
 
-    function isValidMove(piece, idxX, idxY) {
-        if (piece < 0 || piece > 2) return false;
+    function isValidMove(player, idxX, idxY) {
+        if (idxX < 0 || idxX > 2 || idxY < 0 || idxY > 2) return false;
+        if (player < 0 || player > 2) return false;
+        const board = Gameboard.getBoard();
+        
 
         if (board[idxX][idxY] != 0) return false;
-        else if (board[idxX][idxY] == piece) return false;
+        else if (board[idxX][idxY] === player) return false;
         else return true; 
     }
 
-    function horThree(posX, posY, piece) {
+    function horThree(posX, posY, player) {
+        const board = Gameboard.getBoard();
         for (let j = posY, acc = 0; acc < 3; j++, acc++) {
-            if (board[posX][j % 3] != piece) {
+            if (board[posX][j % 3] != player) {
                 return false;
             }
         }
         return true;
     }
 
-    function verThree(posX, posY, piece) {
+    function verThree(posX, posY, player) {
+        const board = Gameboard.getBoard();
         for (let i = posX, acc = 0; acc < 3; i++, acc++) {
-            if (board[i % 3][posY] != piece) {
+            if (board[i % 3][posY] != player) {
                 return false;
             }
         }
         return true;
     }
 
-    function diagThree(posX, posY, piece) {
+    function diagThree(posX, posY, player) {
+        const board = Gameboard.getBoard();
         if (posX === 1 && posY === 1) {
-            if (board[0][0] === piece && board[2][2] === piece) return true;
-            else if (board[0][2] === piece && board[2][0] === piece) return true;
+            if (board[0][0] === player && board[2][2] === player) return true;
+            else if (board[0][2] === player && board[2][0] === player) return true;
         }
         else if (posX === 0 && posY === 0) {
-            if (board[1][1] === piece && board[2][2] === piece) return true;
+            if (board[1][1] === player && board[2][2] === player) return true;
         } 
         else if(posX === 0 && posY === 2) {
-            if (board[1][1] === piece && board[2][0] === piece) return true;
+            if (board[1][1] === player && board[2][0] === player) return true;
         } 
         else if (posX === 2 && posY === 0) {
-            if (board[1][1] === piece && board[0][2] === piece) return true;
+            if (board[1][1] === player && board[0][2] === player) return true;
         } 
         else if (posX === 2 && posY === 2) {
-            if (board[1][1] === piece && board[0][0] === piece) return true;  
+            if (board[1][1] === player && board[0][0] === player) return true;  
+        }
+        return false;
+    }
+
+    function gameWon(lastX, lastY) {   
+        if (lastX === -1 && lastY === -1) return false;     
+        const piece = Gameboard.getBoard()[lastX][lastY];
+        if (horThree(lastX, lastY, piece) || verThree(lastX, lastY, piece) || diagThree(lastX, lastY, piece)) {
+            return true;
         }
         return false;
     }
 
     function boardFull() {
+        const board = Gameboard.getBoard();
         for (let i = 0; i < board.length; i++) {
             for (let j = 0; j < board.length; j++) {
                 if (board[i][j] === 0) return false;
