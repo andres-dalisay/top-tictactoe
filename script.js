451aab8782f723c7e207b680b14dc8256afee28d
andres-dalisay <21106151+andres-dalisay@users.noreply.github.com>
2025-03-18 15:42:25 +0800

    feat: add game win checker

--- script.js
+++ script.js
@@ -27,5 +27,50 @@ const Gameboard = (function () {
         else return true; 
     }
 
+    function horThree(posX, posY, piece) {
+        for (let j = posY, acc = 0; acc < 3; j++, acc++) {
+            if (board[posX][j % 3] != piece) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    function verThree(posX, posY, piece) {
+        for (let i = posX, acc = 0; acc < 3; i++, acc++) {
+            if (board[i % 3][posY] != piece) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    function diagThree(posX, posY, piece) {
+        if (posX === 1 && posY === 1) {
+            if (board[0][0] === piece && board[2][2] === piece) return true;
+            else if (board[0][2] === piece && board[2][0] === piece) return true;
+        }
+        else if (posX === 0 && posY === 0) {
+            if (board[1][1] === piece && board[2][2] === piece) return true;
+        } 
+        else if(posX === 0 && posY === 2) {
+            if (board[1][1] === piece && board[2][0] === piece) return true;
+        } 
+        else if (posX === 2 && posY === 0) {
+            if (board[1][1] === piece && board[0][2] === piece) return true;
+        } 
+        else if (posX === 2 && posY === 2) {
+            if (board[1][1] === piece && board[0][0] === piece) return true;  
+        }
+        return false;
+    }
+
+    function gameWon(lastX, lastY) {        
+        const piece = board[lastX][lastY];
+        if (horThree(lastX, lastY, piece) || verThree(lastX, lastY, piece) || diagThree(lastX, lastY, piece)) {
+            return true;
+        }
+        return false;
+    }
     return {placePiece, getBoard};
 })();
